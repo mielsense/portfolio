879683d240ac2eec46e6ac9d2ead9d8a67e46086
mathis <54747007+underhoney@users.noreply.github.com>
2025-03-25 15:24:20 +0100

    core: initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<main class="flex h-screen max-h-screen max-w-screen items-center justify-center">
+	<div
+		class="bg-muted/20 m-4 flex w-full flex-col items-center justify-center rounded-[20px]"
+		style="height: calc(100vh - 32px); overflow-y: auto;"
+	>
+		{@render children()}
+	</div>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	let art: string = `
+              |\\      _,,,---,,_
+        ZZZzz /,\\\`.-'\\\`'    -.  ;-;;,_
+             |,4-  ) )-,_. ,\\ (  \\\`'-'
+            '---''(\\/--'  \\\`-'\\_)  miel
+     `;
+</script>
+
+<pre class="text-destructive pr-24">{art}</pre>
