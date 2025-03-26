8ad6689e7e200a0eeb0aabd70ea93b464c8963a3
mathis <54747007+underhoney@users.noreply.github.com>
2025-03-26 14:38:46 +0100

    feat: added small animation loop

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,57 @@
 <script lang="ts">
-	let art: string = `
-              |\\      _,,,---,,_
-        ZZZzz /,\\\`.-'\\\`'    -.  ;-;;,_
-             |,4-  ) )-,_. ,\\ (  \\\`'-'
-            '---''(\\/--'  \\\`-'\\_)  miel
-     `;
+	import { onMount } from 'svelte';
+
+	let animationSpeed = 400;
+
+	const frames = [
+		`
+              |\\      *,,,---,,*
+        ZZZzz /,\`.-'\`'    -.  ;-;;,_
+             |,4-  ) )-,_. ,\\ (  \`'-'
+            '---''(\\/--'  \`-'\\_)  miel
+     `,
+		`
+        ZZZ   |\\      *,,,---,,*
+           zz /,\`.-'\`'    -.  ;-;;,_
+             |,4-  ) )-,_. ,\\ (  \`'-'
+            '---''(\\/--'  \`-'\\_)  miel
+     `,
+		`
+           Z  |\\      *,,,---,,*
+        ZZZ   /,\`.-'\`'    -.  ;-;;,_
+             |,4-  ) )-,_. ,\\ (  \`'-'
+            '---''(\\/--'  \`-'\\_)  miel
+     `,
+		`
+              |\\      *,,,---,,*
+        ZZZZZ /,\`.-'\`'    -.  ;-;;,_
+             |,4-  ) )-,_. ,\\ (  \`'-'
+            '---''(\\/--'  \`-'\\_)  miel
+     `,
+		`
+          ZZZ |\\      *,,,---,,*
+            Z /,\`.-'\`'    -.  ;-;;,_
+             |,4-  ) )-,_. ,\\ (  \`'-'
+            '---''(\\/--'  \`-'\\_)  miel
+     `
+	];
+
+	let currentFrameIndex = 0;
+	let currentFrame = frames[currentFrameIndex];
+	let animationInterval: number;
+
+	function nextFrame() {
+		currentFrameIndex = (currentFrameIndex + 1) % frames.length;
+		currentFrame = frames[currentFrameIndex];
+	}
+
+	onMount(() => {
+		animationInterval = setInterval(nextFrame, animationSpeed);
+
+		return () => {
+			clearInterval(animationInterval);
+		};
+	});
 </script>
 
-<pre class="text-destructive -translate-x-[12%]">{art}</pre>
+<pre class="-translate-x-[12%] text-red-200">{currentFrame}</pre>
